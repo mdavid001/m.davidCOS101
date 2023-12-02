cfff4401fcf89a9fe353b688499690c390956d76
m.davidCOS101 <michelle.david@pau.edu.ng>
2023-12-02 23:41:36 +0100

    my eighth commit

--- Week-7/project-2.rs
+++ Week-7/project-2.rs
@@ -1,30 +1,24 @@
+use std::io;
 fn main(){
+	println!("Welcome!");
 	println!("How many siblings do you have", );
 	let mut i1 = String::new();
 	std::io::stdin().read_line(&mut i1).expect("Not a valid string");
-	let a:i32 = i1.trim().parse().expect("Not a valid number");
+	let input1:i32 = i1.trim().parse().expect("Not a valid number");
 
-	loop{
-		println!("Name of sibling : {:?}",a);
-		let mut input2 = String::new();
-	    std::io::stdin().read_line(&mut input2).expect("Not a valid string");
+	let mut input2 = vec![];
 
-		println!("Input 1 if you have more siblings and any other number if you dont");
-		let mut i3 = String::new();
-	    std::io::stdin().read_line(&mut i3).expect("Not a valid string");
-	    let input3:i32 = i3.trim().parse().expect("Not a valid number");
-	    if input3 == 1{
-	    	continue;
-	    }
-	    else{
-	    	println!("The name of your sibling(s) are : {:?}",input3 );
-	    	break;
-	    }
+	for a in 0..input1  {
+		println!("Enter the name of sibling {} : ",a+1);
+		let mut i2 = String::new();
+	    io::stdin().read_line(&mut i2).expect("Not a valid string");
+	    i2 = i2.trim().to_string();
+	    let input2 = i2.trim().to_string();
+	    input2.push(input2)
 
-	}
-
-	if a == 0{
-		println!("Sorry you are not eligible for this program");
 	}
 
 }
+
+	
+	
